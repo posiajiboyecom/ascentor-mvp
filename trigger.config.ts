b523fb9831bf5e85abbcf92087f3a8fdd6940181
Posi Ajiboye <posiajiboye@hotmail.com>
2026-02-20 21:47:26 +0100

    update

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@trigger.dev/sdk/v3";
 
 export default defineConfig({
-  project: "tr_prod_KdtJN4Xamog1RHebzNH4",
+  project: "tr_dev_oqGylBdkxhVVvNFi52R4",
   maxDuration: 300,
   dirs: ["./src/trigger"],
   retries: {
