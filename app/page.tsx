576fedaab50bdafc84199f799184c4bb49aba477
Posi Ajiboye <posiajiboye@hotmail.com>
2026-02-26 21:00:47 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -690,7 +690,7 @@ export default function LandingPage() {
           <div className="lp-footer-top">
             <div>
               <img
-                  src="/ascentor-color-portrait-dark.svg"
+                  src="/ascentor-light-landscape.svg"
                   alt="Ascentor"
                   style={{ height: '28px', width: 'auto', marginBottom: '12px' }}
                 />
